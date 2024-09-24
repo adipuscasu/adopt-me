b84c1b6f8a1688e476aab0a602aeebe48ea67ba6
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2024-09-24 22:34:06 +0300

    error bounary

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
-import Details from "./Details";
+import DetailsErrorBoundary from "./Details";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +21,10 @@ const App = () => {
           <Link to="/">Adapt Me!</Link>
         </header>
         <Routes>
-          <Route path="/details/:id" element={<Details></Details>}></Route>
+          <Route
+            path="/details/:id"
+            element={<DetailsErrorBoundary></DetailsErrorBoundary>}
+          ></Route>
           <Route path="/" element={<SearchParams></SearchParams>}></Route>
         </Routes>
       </QueryClientProvider>
